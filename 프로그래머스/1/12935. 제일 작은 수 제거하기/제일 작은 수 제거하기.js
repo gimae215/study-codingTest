03e5b56548432b5652ecc995ca82ee65ccf56a7e
gimae215 <166392740+gimae215@users.noreply.github.com>
2025-01-17 09:56:20 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 2.29 ms, Memory: 42.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -0,0 +1,10 @@
+function solution(arr) {
+    let min = Math.min(...arr);
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === min) {
+            arr.splice(i, 1);
+            i--;
+        }
+    }
+    return arr.length === 0 ? [-1] : arr;
+}
