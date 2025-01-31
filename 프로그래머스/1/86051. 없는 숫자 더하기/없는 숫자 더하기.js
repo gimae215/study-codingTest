5ffe9577d75f9916ed09cbd231c28c35af782013
gimae215 <166392740+gimae215@users.noreply.github.com>
2025-01-31 16:44:28 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,7 +1,11 @@
 function solution(numbers) {
-    let sum = 0;
-    for(i=0; i<10; i++) {
-        if (!numbers.includes(i)) sum += i;
+    let arr = numbers.sort((a, b) => a - b);
+    let result = 0;
+    for(let i = 0; i<10; i++) {
+        if(numbers[i] !== i) {
+            result += i;
+            numbers.splice(i, 0, i);
+        } 
     }
-    return sum;
+    return result;
 }
